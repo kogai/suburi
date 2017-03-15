6314e98ef356f0fa3dfac0591b6ffb6268e68a23
kogai <kogai0121@gmail.com>
2017-03-15 11:46:54 +0900

    with any

--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ function greet (person: Person): string {
   return 'Hello ' + person.name;
 }
 
+let p: any = { name: 0 };
+
 console.log(greet({name: 'Alice'}));
-console.log(greet({name: false}));
+console.log(greet(p));
