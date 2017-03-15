259034ec1562a72efcc319fcabbe74287e9c79ec
kogai <kogai0121@gmail.com>
2017-03-15 12:48:01 +0900

    real world example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 // @flow
 
-type Person = {
-  name: string;
+type Response = {
+  users: { name: string, age: number }[];
+  count: number;
+  isAvailable: boolean;
 };
 
-function greet (person: Person): string {
-  return 'Hello ' + person.name;
+const server = async (): Promise<Response> => new Promise((resolve) => {
+  let response: any = {
+    users: [
+      { firstName: 'John', familyName: 'Edward', age: 20 }
+    ],
+    usersCount: 100,
+    serviceCount: 20,
+    availability: {
+      status: 'ok',
+    },
+  }
+  setTimeout(() => resolve(response), 1000);
+})
+
+const get = (): Promise<Response> => server();
+
+const consumer = async () => {
+  const {users, count, isAvailable} = await get();
+  console.log('users count is ', count);
+  console.log('service is ', isAvailable);
+  users.forEach(u => {
+    console.log(`user is ${u.name}/${u.age}`);
+  })
 }
 
-let p: any = { name: 0 };
-
-console.log(greet({name: 'Alice'}));
-console.log(greet(p));
+consumer();
