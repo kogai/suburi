0eb5dc2040cc4506b06baa6e5d6f21ffc3982575
kogai <kogai0121@gmail.com>
2017-03-31 21:25:56 +0900

    try thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::thread;
+
 fn main() {
-    println!("hello, world!!!");
+    let x = 1;
+    let handle = thread::spawn(move || {
+        format!("Hello from a thread x is {}", x)
+    });
+    println!("{}", handle.join().unwrap());
 }
