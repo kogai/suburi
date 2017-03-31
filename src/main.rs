cd83566b0feaa05a6d5bf1763ce7b625bad7242b
kogai <kogai0121@gmail.com>
2017-03-31 22:08:40 +0900

    shared variables `

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::thread;
+use std::time::Duration;
+use std::sync::{Arc, Mutex};
 
 fn main() {
-    let x = 1;
-    let handle = thread::spawn(move || {
-        format!("Hello from a thread x is {}", x)
-    });
-    println!("{}", handle.join().unwrap());
+    let xs = Arc::new(Mutex::new(vec![1, 2, 3]));
+
+    for i in 0..3 {
+        let xs_ref = xs.clone();
+        thread::spawn(move || {
+            let mut x = xs_ref.lock().unwrap();
+            x[0] += i;
+        });
+    }
+
+    println!("{:?}", xs);
+    thread::sleep(Duration::from_millis(50));
 }
